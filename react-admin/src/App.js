03fa6189341f1bb948e2e37360cf862decd76e5c
James Xalis <jamesxalis@icloud.com>
2022-12-20 13:34:45 -0700

    team component is complete

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,9 +1,26 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material"
+<<<<<<< HEAD
+=======
+import { Routes, route } from 'react-router-dom'
+import Topbar from './scenes/global/Topbar'
+import Sidebar from './sceens/global/Sidebar'
+import Dashboard from './scenes/dashboard'
+import Team from './scenes/team'
+// import Invoices from './scenes/invoices'
+// import Contacts from './scenes/contacts'
+// import Bar from './scenes/bar'
+// import Form from './scenes/form'
+// import Line from './scenes/line'
+// import Pie from './scenes/pie'
+// import Geography from './scenes/geography'
+// import Calender from './scenes/calender'
+>>>>>>> 5882b934 (team component is complete)
 
 
 function App() {
   const [theme, colorMode] = useMode();
+<<<<<<< HEAD
   return (<ColorModeContext.Provider value={colorMode}>
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -13,6 +30,33 @@ function App() {
     </ThemeProvider>
   </ColorModeContext.Provider>
   );
+=======
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <main className="content">
+          <div className="app">
+            <Topbar />
+            <Routes>
+              <Route path='/' element={<Dashboard/>} />
+              <Route path='/team' element={<Team/>} />
+              {/* <Route path='/contacts' element={<Contacts/>} /> */}
+              {/* <Route path='/invoices' element={<Invoices/>} /> */}
+              {/* <Route path='/form' element={<Form/>} /> */}
+              {/* <Route path='/bar' element={<Bar/>} /> */}
+              {/* <Route path='/pie' element={<Pie/>} /> */}
+              {/* <Route path='/line' element={<Line/>} /> */}
+              {/* <Route path='/faq' element={<Faq/>} /> */}
+              {/* <Route path='/geography' element={<Geography/>} /> */}
+              {/* <Route path='/calendar' element={<Calendar/>} /> */}
+            </Routes>
+          </div>
+        </main>
+      </ThemeProvider>
+    </ColorModeContext.Provider >
+  )
+>>>>>>> 5882b934 (team component is complete)
 }
 
 export default App;
