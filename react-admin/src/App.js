0beec14e35b8373583c7fa74b3fc03fc13a756b5
James Xalis <jamesxalis@icloud.com>
2022-12-20 10:53:54 -0700

    router and routes complete

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,6 +1,18 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material"
+import { Routes, route } from 'react-router-dom'
 import Topbar from './scenes/global/Topbar'
+import Sidebar from './sceens/global/Sidebar'
+import Dashboard from './scenes/dashboard'
+// import Team from './scenes/team'
+// import Invoices from './scenes/invoices'
+// import Contacts from './scenes/contacts'
+// import Bar from './scenes/bar'
+// import Form from './scenes/form'
+// import Line from './scenes/line'
+// import Pie from './scenes/pie'
+// import Geography from './scenes/geography'
+// import Calender from './scenes/calender'
 
 
 function App() {
@@ -9,13 +21,26 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="app">
-          <main className="content">
+        <main className="content">
+          <div className="app">
             <Topbar />
-          </main>
-        </div>
+            <Routes>
+              {/* <Route path='/' element={<Dashboard/>} /> */}
+              {/* <Route path='/team' element={<Team/>} /> */}
+              {/* <Route path='/contacts' element={<Contacts/>} /> */}
+              {/* <Route path='/invoices' element={<Invoices/>} /> */}
+              {/* <Route path='/form' element={<Form/>} /> */}
+              {/* <Route path='/bar' element={<Bar/>} /> */}
+              {/* <Route path='/pie' element={<Pie/>} /> */}
+              {/* <Route path='/line' element={<Line/>} /> */}
+              {/* <Route path='/faq' element={<Faq/>} /> */}
+              {/* <Route path='/geography' element={<Geography/>} /> */}
+              {/* <Route path='/calendar' element={<Calendar/>} /> */}
+            </Routes>
+          </div>
+        </main>
       </ThemeProvider>
-    </ColorModeContext.Provider>
+    </ColorModeContext.Provider >
   )
 }
 
