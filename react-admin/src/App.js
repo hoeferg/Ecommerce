a1a999d365c6f0dc6958b1a1864da0d73132512c
Gayle Hoefer <gaylehoefer@gmail.com>
2022-12-20 08:42:06 -0700

    light and dark mode set up done

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,11 +1,17 @@
-import Topbar from './scenes/global/Topbar'
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material"
 
 
 function App() {
-  return (
-    <div className="app">
-      <Topbar />
-    </div>
+  const [theme, colorMode] = useMode();
+  return (<ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="app">
+        <main className="content"></main>
+      </div>
+    </ThemeProvider>
+  </ColorModeContext.Provider>
   );
 }
 
