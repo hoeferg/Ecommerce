2764c35f443e52ec71d612eb1be07eb8386556d9
Gayle Hoefer <gaylehoefer@gmail.com>
2022-12-20 10:11:02 -0700

    resolved merge conflicts

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,29 +1,24 @@
-<<<<<<< HEAD
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material"
-
-
-function App() {
-  const [theme, colorMode] = useMode();
-  return (<ColorModeContext.Provider value={colorMode}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div className="app">
-        <main className="content"></main>
-      </div>
-    </ThemeProvider>
-  </ColorModeContext.Provider>
-=======
 import Topbar from './scenes/global/Topbar'
 
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-      <Topbar />
-    </div>
->>>>>>> 044290fdebbfba9565bec2479d74c2cdc8404bfe
-  );
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
 }
+
+
 
 export default App;
