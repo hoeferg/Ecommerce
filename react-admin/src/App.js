aede09410812a84acb7049c6cb647c7754a55ab1
James Xalis <jamesxalis@icloud.com>
2022-12-22 11:04:30 -0700

    fixed webpack issues

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { ColorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material"
+import { CssBaseline } from "@mui/material"
+import { ThemeProvider } from '@mui/material/styles';
 import { Routes, Route } from 'react-router-dom'
 import Sidebar from './scenes/global/Sidebar'
 import Topbar from './scenes/global/Topbar'
@@ -24,25 +25,25 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
-          <div className="app">
-            <Sidebar isSidebar= {isSidebar} />
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              <Route path='/' element={<Dashboard/>} />
-              <Route path='/team' element={<Team/>} />
-              <Route path='/contacts' element={<Contacts/>} />
-              <Route path='/invoices' element={<Invoices/>} />
-              <Route path='/form' element={<Form/>} />
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/team' element={<Team />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/form' element={<Form />} />
               {/* <Route path='/bar' element={<Bar/>} /> */}
               {/* <Route path='/pie' element={<Pie/>} /> */}
               {/* <Route path='/line' element={<Line/>} /> */}
               {/* <Route path='/faq' element={<Faq/>} /> */}
               {/* <Route path='/geography' element={<Geography/>} /> */}
-              <Route path='/calendar' element={<Calendar/>} />
+              <Route path='/calendar' element={<Calendar />} />
             </Routes>
-          </div>
           </main>
+        </div>
       </ThemeProvider>
     </ColorModeContext.Provider >
   )
--- react-admin/src/scenes/invoices/index.jsx
+++ react-admin/src/scenes/invoices/index.jsx
@@ -1,7 +1,7 @@
 import { Box, Typography, useTheme } from '@mui/material'
 import { DataGrid, } from '@mui/x-data-grid'
 import { tokens } from '../../theme'
-import { mockDataContacts } from '../../data/mockData'
+import { mockDataInvoices } from '../../data/mockData'
 import Header from '../../components/Header';
 
 const Invoices = () => {
