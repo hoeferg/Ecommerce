0a3953b34b027ea6ca42f2eeba174fc9fc510a44
Gayle Hoefer <gaylehoefer@gmail.com>
2022-12-22 10:27:17 -0700

    save merge

--- react-admin/src/scenes/form/index.jsx
+++ react-admin/src/scenes/form/index.jsx
@@ -1,33 +1,10 @@
 import { Box, Button, TextField } from "@mui/material";
 import { Formik } from "formik";
-import * as yup from "yup";
+import { object, string} from 'yup';
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 
 
-const initialValues = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    contact: "",
-    address1: "",
-    address2: "",
-};
-
-const phoneRegExp = /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
-
-const userSchema = 
-yup.object().shape({
-    firstName: yup.string().required("required"),
-    lastName: yup.string().required("required"),
-    email: yup.string().email("invalid email").required("required"),
-    Contact: yup
-        .string()
-        .matches(phoneRegExp, "phone number is not valid")
-        .required("required"),
-    address1: yup.string().required("required"),
-    address2: yup.string().required("required"),
-})
 
 const Form = () => {
     const isNonMobile = useMediaQuery("(min-width:600px)");
@@ -38,10 +15,11 @@ const Form = () => {
 
     return <Box m="20px">
         <Header title="CREATE USER" subtitle="Create a New User Profile" />
+
         <Formik
             onSubmit={handleFormSubmit}
             initialValues={initialValues}
-            validationSchema={userSchema}
+            validationSchema={checkoutSchema}
         >
             {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
                 <form onSubmit={handleSubmit}>
@@ -146,7 +124,29 @@ const Form = () => {
             )}
         </Formik>
     </Box>
+};
 
-}
+const phoneRegExp =
+    /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const checkoutSchema = object().shape({
+    firstName: string().required("required"),
+    lastName: string().required("required"),
+    email: string().email("invalid email").required("required"),
+    contact: 
+        string()
+        .matches(phoneRegExp, "Phone number is not valid")
+        .required("required"),
+    address1: string().required("required"),
+    address2: string().required("required"),
+});
+const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    contact: "",
+    address1: "",
+    address2: "",
+};
 
 export default Form;
