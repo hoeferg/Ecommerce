6da2f0a4e3367a361d9045459e63a297a5becaa5
Gayle Hoefer <gaylehoefer@gmail.com>
2022-12-22 11:03:26 -0700

    fixed theme issue

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -24,25 +24,25 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
-          <div className="app">
-            <Sidebar isSidebar= {isSidebar} />
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              <Route path='/' element={<Dashboard/>} />
-              <Route path='/team' element={<Team/>} />
-              <Route path='/contacts' element={<Contacts/>} />
-              <Route path='/invoices' element={<Invoices/>} />
-              <Route path='/form' element={<Form/>} />
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/team' element={<Team />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/form' element={<Form />} />
               {/* <Route path='/bar' element={<Bar/>} /> */}
               {/* <Route path='/pie' element={<Pie/>} /> */}
               {/* <Route path='/line' element={<Line/>} /> */}
               {/* <Route path='/faq' element={<Faq/>} /> */}
               {/* <Route path='/geography' element={<Geography/>} /> */}
-              <Route path='/calendar' element={<Calendar/>} />
+              <Route path='/calendar' element={<Calendar />} />
             </Routes>
-          </div>
           </main>
+        </div>
       </ThemeProvider>
     </ColorModeContext.Provider >
   )
