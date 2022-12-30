99b6bf76726f43780e19594032b534b44b2c64f5
James Xalis <jamesxalis@icloud.com>
2022-12-30 12:34:54 -0700

    created barchart

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -11,7 +11,7 @@ import Contacts from './scenes/contacts'
 import Invoices from './scenes/invoices'
 import Calendar from './scenes/calendar'
 import Form from './scenes/form'
-// import Bar from './scenes/bar'
+import Bar from './scenes/bar'
 // import Line from './scenes/line'
 // import Pie from './scenes/pie'
 // import Geography from './scenes/geography'
@@ -41,7 +41,7 @@ function App() {
               <Route path='/contacts' element={<Contacts />} />
               <Route path='/invoices' element={<Invoices />} />
               <Route path='/form' element={<Form />} />
-              {/* <Route path='/bar' element={<Bar/>} /> */}
+              <Route path='/bar' element={<Bar/>} />
               {/* <Route path='/pie' element={<Pie/>} /> */}
               {/* <Route path='/line' element={<Line/>} /> */}
               {/* <Route path='/faq' element={<Faq/>} /> */}
