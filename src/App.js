a181ba70b043c210b8a4f70b1ae0f031811bcca6
Gayle Hoefer <gaylehoefer@gmail.com>
2022-12-30 13:01:04 -0700

    added imports of dashboard

--- src/App.js
+++ src/App.js
@@ -38,15 +38,12 @@ function App() {
               <Route path='/form' element={<Form />} />
               <Route path='/' element={<Dashboard />} />
               <Route path='/team' element={<Team />} />
-              <Route path='/contacts' element={<Contacts />} />
-              <Route path='/invoices' element={<Invoices />} />
               <Route path='/form' element={<Form />} />
               <Route path='/faq' element={<FAQ/>} />
               {/* <Route path='/bar' element={<Bar/>} /> */}
               {/* <Route path='/pie' element={<Pie/>} /> */}
               {/* <Route path='/line' element={<Line/>} /> */}
               {/* <Route path='/geography' element={<Geography/>} /> */}
-              <Route path='/calendar' element={<Calendar />} />
               <Route path='/calendar' element={<Calendar />} />
             </Routes>
           </main>
--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,8 +1,21 @@
-import { Box } from "@mui/material";
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme"
 import Header from "../../components/Header";
+import { mockTranscations } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointofSaleIcon from "@mui/icons-material/PointofSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/TrafficOutlined";
+import LineChart from "../../components/LineChart";
+import BarChart from "../../components/BarChart";
+import GeographyChart from "../../components/GeographyChart";
+import StatChart from "../../components/StatChart";
+import ProgressChart from "../../components/ProgressChart";
 
 const Dashboard =() => {
-    return <Box m="20px">
+    return (
+    <Box m="20px">
         <Box 
         display="flex"
         justifyContent="space-between"
@@ -11,6 +24,7 @@ const Dashboard =() => {
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
         </Box>
     </Box>
-}
+    );
+};
 
 export default Dashboard;
